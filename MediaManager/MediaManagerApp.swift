03469ed9f7e2dd80cf1395f0d3a482e971ced130
larryb02 <larrykbanks21@gmail.com>
2023-10-15 19:03:37 -0400

    Initial Commit

---
+++ MediaManager/MediaManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaManagerApp.swift
+//  MediaManager
+//
+//  Created by Larry Banks on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MediaManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
