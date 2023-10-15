4f34129eb1a37478aac24ddbaa232789731871a0
larryb02 <larrykbanks21@gmail.com>
2023-10-15 19:16:54 -0400

    xcode changed some stuff up

--- MediaManager/MediaManagerApp.swift
+++ MediaManager/MediaManagerApp.swift
@@ -8,10 +8,34 @@
 import SwiftUI
 
 @main
-struct MediaManagerApp: App {
+struct EntryPoint: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+    //potentially login screen before this
+    MainMenu()
     }
 }
+//struct Login: View
+struct MainMenu: Scene {
+    var body: some Scene{
+        WindowGroup{
+            TabView{
+                ContentView() //Eventually a menu that displays books,games,etc at random
+                                //or by most popular etc...
+                .tabItem{
+                    Label("Explore", systemImage: "globe")
+                }
+                ContentView() //Users personal manager
+                .tabItem{
+                    Label("Manage", systemImage: "book")
+                }
+                ContentView() //Settings...
+                .tabItem{
+                    Label("Settings", systemImage: "gear")
+                }
+            }
+        }
+    }
+
+}
+
+
