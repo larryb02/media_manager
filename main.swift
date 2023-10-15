4a865afb6936576cdfcc32173d26ed9f1daa1bbf
larryb02 <larrykbanks21@gmail.com>
2023-10-15 17:31:01 -0400

    update to structure

--- main.swift
+++ main.swift
@@ -1,11 +1,32 @@
 import SwiftUI
 
 @main
-struct MainMenu: App {
+struct EntryPoint: App {
     var body: some Scene{
-        Window ("filler TODO: change later") {
+    MainMenu()
+    }
+}
 
+struct MainMenu: View {
+    var body: some Scene{
+        WindowGroup{
+            TabView{
+                ContentView() //Eventually a menu that displays books,games,etc at random 
+                                //or by most popular etc...
+                .tabItem{
+                    Label("Explore")
+                } 
+                ContentView() //Users personal manager
+                .tabItem{
+                    Label("Manage")
+                }
+                SettingsView() //Settings...
+                .tabItem{
+                    Label("Settings", systemImage: "gear")
+                }
+            }
         }
     }
 
 }
+
